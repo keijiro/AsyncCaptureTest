ac453817e35027863230448e08d0eae50c5539c8
Keijiro Takahashi <keijiro@gmail.com>
2020-07-04 10:54:14 +0900

    Update with Unity 2019.4

--- Assets/AsyncCapture.cs
+++ Assets/AsyncCapture.cs
@@ -1,62 +1,34 @@
 ﻿using UnityEngine;
-using Unity.Collections;
-using System.IO;
-using System.Collections.Generic;
-
-#if UNITY_2018_3_OR_NEWER
 using UnityEngine.Rendering;
-#else
-using UnityEngine.Experimental.Rendering;
-#endif
+using System.IO;
+using System.Collections;
 
 public class AsyncCapture : MonoBehaviour
 {
-    Queue<AsyncGPUReadbackRequest> _requests = new Queue<AsyncGPUReadbackRequest>();
-
-    void Update()
+    IEnumerator Start()
     {
-        while (_requests.Count > 0)
+        while (true)
         {
-            var req = _requests.Peek();
+            yield return new WaitForSeconds(1);
+            yield return new WaitForEndOfFrame();
 
-            if (req.hasError)
-            {
-                Debug.Log("GPU readback error detected.");
-                _requests.Dequeue();
-            }
-            else if (req.done)
-            {
-                var buffer = req.GetData<Color32>();
-
-                if (Time.frameCount % 60 == 0)
-                {
-                    var camera = GetComponent<Camera>();
-                    SaveBitmap(buffer, camera.pixelWidth, camera.pixelHeight);
-                }
-
-                _requests.Dequeue();
-            }
-            else
-            {
-                break;
-            }
+            var rt = RenderTexture.GetTemporary(Screen.width, Screen.height, 0);
+            ScreenCapture.CaptureScreenshotIntoRenderTexture(rt);
+            AsyncGPUReadback.Request(rt, 0, TextureFormat.RGBA32, OnCompleteReadback);
+            RenderTexture.ReleaseTemporary(rt);
         }
     }
 
-    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    void OnCompleteReadback(AsyncGPUReadbackRequest request)
     {
-        if (_requests.Count < 8)
-            _requests.Enqueue(AsyncGPUReadback.Request(source));
-        else
-            Debug.Log("Too many requests.");
+        if (request.hasError)
+        {
+            Debug.Log("GPU readback error detected.");
+            return;
+        }
 
-        Graphics.Blit(source, destination);
-    }
-
-    void SaveBitmap(NativeArray<Color32> buffer, int width, int height)
-    {
-        var tex = new Texture2D(width, height, TextureFormat.RGBA32, false);
-        tex.SetPixels32(buffer.ToArray());
+        var tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGBA32, false);
+        tex.SetPixels32(request.GetData<Color32>().ToArray());
         tex.Apply();
         File.WriteAllBytes("test.png", ImageConversion.EncodeToPNG(tex));
         Destroy(tex);
