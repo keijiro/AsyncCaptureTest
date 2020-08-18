162ca75b2ef1607e25d969d1b5affefc23dd0ec3
Keijiro Takahashi <keijiro@gmail.com>
2020-08-18 17:53:26 +0900

    Change to use LoadRawTextureData

--- Assets/AsyncCapture.cs
+++ Assets/AsyncCapture.cs
@@ -12,9 +12,9 @@ public class AsyncCapture : MonoBehaviour
             yield return new WaitForSeconds(1);
             yield return new WaitForEndOfFrame();
 
-            var rt = RenderTexture.GetTemporary(Screen.width, Screen.height, 0);
+            var rt = RenderTexture.GetTemporary(Screen.width, Screen.height, 0, RenderTextureFormat.ARGB32);
             ScreenCapture.CaptureScreenshotIntoRenderTexture(rt);
-            AsyncGPUReadback.Request(rt, 0, TextureFormat.RGBA32, OnCompleteReadback);
+            AsyncGPUReadback.Request(rt, 0, TextureFormat.ARGB32, OnCompleteReadback);
             RenderTexture.ReleaseTemporary(rt);
         }
     }
@@ -27,8 +27,8 @@ public class AsyncCapture : MonoBehaviour
             return;
         }
 
-        var tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGBA32, false);
-        tex.SetPixels32(request.GetData<Color32>().ToArray());
+        var tex = new Texture2D(Screen.width, Screen.height, TextureFormat.ARGB32, false);
+        tex.LoadRawTextureData(request.GetData<uint>());
         tex.Apply();
         File.WriteAllBytes("test.png", ImageConversion.EncodeToPNG(tex));
         Destroy(tex);
