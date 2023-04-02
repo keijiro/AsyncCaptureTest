5164c4a939ed16557892a7a14d2bf1aad738dbb5
Keijiro Takahashi <keijiro@gmail.com>
2023-04-02 11:47:26 +0900

    comments

--- Assets/AsyncCapture.cs
+++ Assets/AsyncCapture.cs
@@ -20,19 +20,21 @@ public sealed class AsyncCapture : MonoBehaviour
         var flipRT = new RenderTexture(w, h, 0);
         var rtFormat = flipRT.graphicsFormat;
 
-        var buffer = new NativeArray<byte>(w * h * 4, Allocator.Persistent,
-                                           NativeArrayOptions.UninitializedMemory);
+        var buffer = new NativeArray<byte>(w * h * 4, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
 
         try
         {
             for (var cancel = destroyCancellationToken;;)
             {
+                // Interval (cancellable)
                 await Awaitable.WaitForSecondsAsync(_interval, cancel);
                 await Awaitable.EndOfFrameAsync(cancel);
 
+                // Screen capture
                 ScreenCapture.CaptureScreenshotIntoRenderTexture(grabRT);
                 Graphics.Blit(grabRT, flipRT, scale, offs);
 
+                // Asynchronous GPU readback
                 var req = await AsyncGPUReadback.RequestIntoNativeArrayAsync(ref buffer, flipRT, 0);
 
                 if (req.hasError)
@@ -41,13 +43,17 @@ public sealed class AsyncCapture : MonoBehaviour
                     continue;
                 }
 
+                // Check if it's cancelled before going background.
+                cancel.ThrowIfCancellationRequested();
+
+                // PNG encoding in background: The ImageConversion methods are
+                // thread safe (documented on the scripting reference), so we
+                // can run it in a background thread.
                 await Awaitable.BackgroundThreadAsync();
-
-                using var encoded = ImageConversion.
-                  EncodeNativeArrayToPNG(buffer, rtFormat, (uint)w, (uint)h);
-
+                using var encoded = ImageConversion.EncodeNativeArrayToPNG(buffer, rtFormat, (uint)w, (uint)h);
                 await Awaitable.MainThreadAsync();
 
+                // Asynchronous file output
                 await File.WriteAllBytesAsync(_outputFileName, encoded.ToArray(), cancel);
             }
         }
@@ -56,6 +62,9 @@ public sealed class AsyncCapture : MonoBehaviour
         }
         finally
         {
+            // Note: We sould clean the resources up very carefully because this
+            // block can be run after OnDestroy; Unity objects would have been
+            // recycled by the engine.
             if (flipRT != null) Destroy(flipRT);
             if (grabRT != null) Destroy(grabRT);
             if (buffer.IsCreated) buffer.Dispose();
